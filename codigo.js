745b613c5cfd169e15c6b90964046fc657073da2
Heloyse Silva <sraheloysesantos@gmail.com>
2024-09-11 16:03:15 -0300

    Create codigo.js

---
+++ codigo.js
@@ -0,0 +1,23 @@
+//este codigo foi para treino de variavel
+
+const rotulo = "Natalia";
+console.log(rotulo);
+
+console.log('Hello'+' '+'World')
+myName = 'Maria'
+console.log('Hello '+ myName)
+myName2 = "2"
+console.log(`Hello ${myName2}`)
+
+const numero1 = 50;
+const numero2 = 50;
+const soma = numero1 + numero2;
+const mult = numero1 * numero2;
+const div = numero1 / numero2;
+const sub = numero1 - numero2;
+console.log(`O resultado da soma entre n1 e n2 é: ${numero1} + ${numero2} = ${soma}\n
+O resultado da multiplicação entre n1 e n2 é: ${numero1} * ${numero2} = ${mult}\n
+O resultado da divisão entre n1 e n2 é: ${numero1} / ${numero2} = ${div}\n
+O resultado da subtração entre n1 e n2 é: ${numero1} - ${numero2} = ${sub}`
+
+)
