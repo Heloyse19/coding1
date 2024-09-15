bf11545caf5f68dd9893351aa20538a273be5311
Heloyse Silva <sraheloysesantos@gmail.com>
2024-09-14 23:30:44 -0300

    atividade.js

---
+++ atividade.js
@@ -0,0 +1,28 @@
+const nome = "Heloyse";
+const idade = "19";
+const cidade = "Paulista";
+console.log("Olá, meu nome é "+nome+" e tenho "+idade);
+console.log(nome+" é da cidade de "+cidade);
+
+const numero1 = 50;
+const numero2 = 50;
+const soma = numero1 + numero2;
+const mult = numero1 * numero2;
+const div = numero1 / numero2;
+const sub = numero1 - numero2;
+console.log(`O resultado da soma entre n1 e n2 é: ${numero1} + ${numero2} = ${soma}\n
+O resultado da multiplicação entre n1 e n2 é: ${numero1} * ${numero2} = ${mult}\n
+O resultado da divisão entre n1 e n2 é: ${numero1} / ${numero2} = ${div}\n
+O resultado da subtração entre n1 e n2 é: ${numero1} - ${numero2} = ${sub}\n`
+)
+
+const base = 10;
+const altura = 10;
+const area = (base*altura)/2;
+console.log("O resultado da area do triangulo é: "+area);
+
+const nota1 = 6;
+const nota2 = 10;
+const nota3 = 10;
+const media = (nota1+nota2+nota3)/3
+console.log("O resultado da média é: "+media)
