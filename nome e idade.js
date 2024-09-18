ac7e132036cf7a76f4869a47317a44ce0a13705a
Heloyse Silva <sraheloysesantos@gmail.com>
2024-09-18 15:57:57 -0300

    Delete nome e idade.js

--- nome e idade.js
+++
@@ -1,6 +0,0 @@
-//variavel nome
-const nome = "Heloyse";
-const idade = "19";
-const cidade = "Paulista";
-console.log("Olá, meu nome é "+nome+" e tenho "+idade);
-console.log(nome+" é da cidade de "+cidade);
