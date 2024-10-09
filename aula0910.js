7a9264b48eabcfa082b92a8c8aa2d4abc00b4daa
Heloyse Silva <sraheloysesantos@gmail.com>
2024-10-09 14:29:14 -0300

    Create aula0910.js

---
+++ aula0910.js
@@ -0,0 +1,13 @@
+let nome= prompt("Qual seu nome?: ")
+let pesquisa= prompt("Escolha a fruta")
+
+switch (pesquisa){
+  case "limão":
+    console.log(`Olá ${nome}, o limão custa R$2,00`)
+    break;
+  case "morango":
+    console.log(`Olá ${nome}, o morango custa R$5,00`)
+    break;
+  default:
+    console.log(`A opção: ${pesquisa} não existe`)
+}
